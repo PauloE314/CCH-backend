99fedf5a09a36ce9544e2e5feb4fed9839b5b534
Paulo Eduardo <paulo.eduardo@codeminer42.com>
2021-10-28 16:28:18 -0300

    Bug fix on socketTest

--- test/helpers.ts
+++ test/helpers.ts
@@ -20,14 +20,16 @@ type TestSocketCallBack = (socket: Socket, resolve: () => any) => any;
 export function testSocket(message: string, cb: TestSocketCallBack) {
   it(message, done => {
     const client = connect(`http://localhost:${port}`);
-    try {
-      cb(client, () => {
-        client.close();
-        done();
-      });
-    } catch (error) {
+    const end = (err?: any) => {
       client.close();
-      done(error);
+      done(err);
+    };
+
+    try {
+      const resp = cb(client, end);
+      if (resp instanceof Promise) resp.catch(end);
+    } catch (error) {
+      end(error);
     }
   });
 }

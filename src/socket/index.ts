363c54ac33fda72e2056899f99b9fd020604b85a
Paulo Eduardo <paulo.eduardo@codeminer42.com>
2021-11-05 16:22:50 -0300

    Removes 'newPlayer' listener

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -3,7 +3,6 @@ import { Server as SocketServer } from 'socket.io';
 import setupPlayer from './middlewares/setupPlayer';
 
 import { ISocketStorage } from './storage/ISocketStorage';
-import newPlayer from './listeners/newPlayer';
 import disconnect from './listeners/disconnect';
 import newParty from './listeners/newParty';
 import joinParty from './listeners/joinParty';
@@ -18,7 +17,6 @@ export default function setupWebSockets(
   io.use((socket, next) => setupPlayer(socket, storage, next));
 
   io.on('connection', socket => {
-    socket.on('new-player', data => newPlayer(io, socket, storage, data));
     socket.on('disconnect', data => disconnect(io, socket, storage, data));
     socket.on('new-party', data => newParty(io, socket, storage, data));
     socket.on('join-party', data => joinParty(io, socket, storage, data));

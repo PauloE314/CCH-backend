2886274ea1a648f4f6871dccb4cc6d40724f262d
Paulo <paulo.eduardo@codeminer42.com>
2021-12-01 18:41:53 -0300

    Renames Storage (-> GameStorage)

--- src/socket/storage/index.ts
+++ src/socket/storage/index.ts
@@ -1,15 +1,7 @@
 import { Party } from '../models/Party';
 import { Player } from '../models/Player';
 import { DataMap } from './DataMap';
-
-interface SocketStorage {
-  players: DataMap<Player>;
-  parties: DataMap<Party>;
-
-  drop(): void;
-}
-
-class Storage implements SocketStorage {
+class GameStorage {
   public players: DataMap<Player>;
   public parties: DataMap<Party>;
 
@@ -24,4 +16,4 @@ class Storage implements SocketStorage {
   }
 }
 
-export { Storage, SocketStorage };
+export { GameStorage };
